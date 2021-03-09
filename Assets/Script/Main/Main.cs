4aa6f84f8466f68fab3b73d37a1231b76955a68e
Coreficent <coreficent@gmail.com>
2021-03-09 11:31:27 -0500

    Fix river conjunction.

--- Assets/Script/Main/Main.cs
+++ Assets/Script/Main/Main.cs
@@ -33,7 +33,8 @@
         {
             Land,
             Tower,
-            Road
+            River,
+            Vista
         }
 
         private GenerationState state = GenerationState.Land;
@@ -70,30 +71,32 @@
                         }
                         else
                         {
-                            timeGap = 1.0f;
+                            timeGap = 0.25f;
                             Vector2Int position = tileFactory.Random();
                             queue.Enqueue(new Tuple<Vector2Int, Genesis>(position, River1)); ;
                             set.Add(position);
-                            state = GenerationState.Road;
+                            state = GenerationState.River;
                         }
 
                         --towerCount;
 
                         break;
 
-                    case GenerationState.Road:
+                    case GenerationState.River:
 
                         if (queue.Count > 0)
                         {
                             var rootedGenesis = queue.Dequeue();
-                            Vector2Int posision = rootedGenesis.Item1;
+                            Vector2Int position = rootedGenesis.Item1;
                             Genesis genesis = rootedGenesis.Item2;
 
-                            tileFactory.PlaceTile(posision, genesis);
+                            tileFactory.PlaceTile(position, genesis);
 
                             if (genesis.North.Count > 0)
                             {
-                                Vector2Int newPosition = new Vector2Int(posision.x, posision.y + 1);
+                                float offset = 1.0f;
+                                float angleOffset = 0.0f;
+                                Vector2Int newPosition = new Vector2Int(position.x + Mathf.RoundToInt(Mathf.Sin((transform.eulerAngles.y + angleOffset) * Mathf.Deg2Rad) * offset), position.y + Mathf.RoundToInt(Mathf.Cos((transform.eulerAngles.y + angleOffset) * Mathf.Deg2Rad) * offset));
                                 if (tileFactory.ValidRange(newPosition))
                                 {
                                     if (!set.Contains(newPosition))
@@ -103,33 +106,12 @@
                                     }
                                 }
                             }
-                            if (genesis.South.Count > 0)
-                            {
-                                Vector2Int newPosition = new Vector2Int(posision.x, posision.y - 1);
-                                if (tileFactory.ValidRange(newPosition))
-                                {
-                                    if (!set.Contains(newPosition))
-                                    {
-                                        queue.Enqueue(new Tuple<Vector2Int, Genesis>(newPosition, genesis.South[UnityEngine.Random.Range(0, genesis.South.Count)]));
-                                        set.Add(newPosition);
-                                    }
-                                }
-                            }
-                            if (genesis.West.Count > 0)
-                            {
-                                Vector2Int newPosition = new Vector2Int(posision.x - 1, posision.y);
-                                if (tileFactory.ValidRange(newPosition))
-                                {
-                                    if (!set.Contains(newPosition))
-                                    {
-                                        queue.Enqueue(new Tuple<Vector2Int, Genesis>(newPosition, genesis.West[UnityEngine.Random.Range(0, genesis.West.Count)]));
-                                        set.Add(newPosition);
-                                    }
-                                }
-                            }
+
                             if (genesis.East.Count > 0)
                             {
-                                Vector2Int newPosition = new Vector2Int(posision.x + 1, posision.y); if (tileFactory.ValidRange(newPosition))
+                                float offset = 1.0f;
+                                float angleOffset = 90.0f;
+                                Vector2Int newPosition = new Vector2Int(position.x + Mathf.RoundToInt(Mathf.Sin((transform.eulerAngles.y + angleOffset) * Mathf.Deg2Rad) * offset), position.y + Mathf.RoundToInt(Mathf.Cos((transform.eulerAngles.y + angleOffset) * Mathf.Deg2Rad) * offset));
                                 {
                                     if (tileFactory.ValidRange(newPosition))
                                     {
@@ -141,9 +123,53 @@
                                     }
                                 }
                             }
+                            if (genesis.South.Count > 0)
+                            {
+                                float offset = 1.0f;
+                                float angleOffset = 180.0f;
+                                Vector2Int newPosition = new Vector2Int(position.x + Mathf.RoundToInt(Mathf.Sin((transform.eulerAngles.y + angleOffset) * Mathf.Deg2Rad) * offset), position.y + Mathf.RoundToInt(Mathf.Cos((transform.eulerAngles.y + angleOffset) * Mathf.Deg2Rad) * offset));
+
+                                if (tileFactory.ValidRange(newPosition))
+                                {
+                                    if (!set.Contains(newPosition))
+                                    {
+                                        queue.Enqueue(new Tuple<Vector2Int, Genesis>(newPosition, genesis.South[UnityEngine.Random.Range(0, genesis.South.Count)]));
+                                        set.Add(newPosition);
+                                    }
+                                }
+                            }
+                            if (genesis.West.Count > 0)
+                            {
+                                float offset = 1.0f;
+                                float angleOffset = 270.0f;
+                                Vector2Int newPosition = new Vector2Int(position.x + Mathf.RoundToInt(Mathf.Sin((transform.eulerAngles.y + angleOffset) * Mathf.Deg2Rad) * offset), position.y + Mathf.RoundToInt(Mathf.Cos((transform.eulerAngles.y + angleOffset) * Mathf.Deg2Rad) * offset));
+
+                                DebugUtility.Bug("go west", newPosition);
+
+                                if (tileFactory.ValidRange(newPosition))
+                                {
+                                    if (!set.Contains(newPosition))
+                                    {
+                                        queue.Enqueue(new Tuple<Vector2Int, Genesis>(newPosition, genesis.West[UnityEngine.Random.Range(0, genesis.West.Count)]));
+                                        set.Add(newPosition);
+                                    }
+                                }
+                            }
+
+                        }
+                        else
+                        {
+                            state = GenerationState.Vista;
                         }
 
                         break;
+
+                    case GenerationState.Vista:
+
+                        DebugUtility.Log("Vista");
+
+                        break;
+
                     default:
                         DebugUtility.Warn("unexpected generation state");
                         break;
