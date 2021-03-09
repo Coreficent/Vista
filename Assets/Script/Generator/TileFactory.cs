4e9beb79e2220dfe96eaa15964bff528ba3dbde3
Coreficent <coreficent@gmail.com>
2021-03-08 20:21:48 -0500

    Implement board storage.

--- Assets/Script/Generator/TileFactory.cs
+++ Assets/Script/Generator/TileFactory.cs
@@ -1,6 +1,7 @@
 ﻿namespace Coreficent.Generator
 {
     using Coreficent.Utility;
+    using System;
     using System.Collections.Generic;
     using UnityEngine;
 
@@ -10,7 +11,9 @@
         public int size;
 
         private int index = 0;
-        private List<Vector2> positions = new List<Vector2>();
+        private List<Tuple<int, int>> positions = new List<Tuple<int, int>>();
+
+        private Genesis[,] board;
 
         public void Start()
         {
@@ -18,9 +21,10 @@
             {
                 for (var y = 0; y < size; ++y)
                 {
-                    positions.Add(new Vector2(x - size / 2, y - size / 2));
+                    positions.Add(new Tuple<int, int>(x - size / 2, y - size / 2));
                 }
             }
+            board = new Genesis[size, size];
         }
 
         public bool HasNext()
@@ -28,14 +32,14 @@
             return index < positions.Count;
         }
 
-        public Vector2 Next()
+        public Tuple<int, int> Next()
         {
             return positions[index++];
         }
 
-        public void NextTile(Vector2 position)
+        public void NextTile(Tuple<int, int> position)
         {
-            Instantiate(Tile, new Vector3(position.x, 0.0f, position.y), Quaternion.identity);
+            board[position.Item1, position.Item2] = Instantiate(Tile, new Vector3(position.Item1, 0.0f, position.Item2), Quaternion.identity);
         }
     }
 }
