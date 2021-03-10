9a513d3c04f32b3baec281bbf5a286073cb096b4
Coreficent <coreficent@gmail.com>
2021-03-10 13:32:58 -0500

    Improve track weight.

--- Assets/Script/Tile/RiverStraight.cs
+++ Assets/Script/Tile/RiverStraight.cs
@@ -9,16 +9,24 @@
         {
             List<Neighbor> neighbors = new List<Neighbor>();
 
+            Neighbor northStraightRiver = new Neighbor() { Offset = RotateOffset(1.0f, Neighbor.North), Tile = factory.RiverStraight, Rotation = CumulateRotation(0.0f) };
+
             Neighbor[] north = new Neighbor[] {
-                    new Neighbor() { Offset = RotateOffset(1.0f, Neighbor.North), Tile = factory.RiverStraight, Rotation = CumulateRotation(0.0f) },
+                    northStraightRiver,
+                    northStraightRiver,
+                    northStraightRiver,
                     new Neighbor() { Offset = RotateOffset(1.0f, Neighbor.North), Tile = factory.RiverCorner, Rotation = CumulateRotation(90.0f) },
                     new Neighbor() { Offset = RotateOffset(1.0f, Neighbor.North), Tile = factory.RiverCorner, Rotation = CumulateRotation(180.0f) }
                 };
 
             neighbors.Add(Pick(north));
 
+            Neighbor southStraightRiver = new Neighbor() { Offset = RotateOffset(1.0f, Neighbor.South), Tile = factory.RiverStraight, Rotation = CumulateRotation(0.0f) };
+
             Neighbor[] south = new Neighbor[] {
-                    new Neighbor() { Offset = RotateOffset(1.0f, Neighbor.South), Tile = factory.RiverStraight, Rotation = CumulateRotation(0.0f)},
+                    southStraightRiver,
+                    southStraightRiver,
+                    southStraightRiver,
                     new Neighbor() { Offset = RotateOffset(1.0f, Neighbor.South), Tile = factory.RiverCorner, Rotation = CumulateRotation(0.0f) },
                     new Neighbor() { Offset = RotateOffset(1.0f, Neighbor.South), Tile = factory.RiverCorner, Rotation = CumulateRotation(270.0f) }
                 };
@@ -26,7 +34,6 @@
             neighbors.Add(Pick(south));
 
             return neighbors;
-
         }
     }
 }
--- Assets/Script/Tile/RoadStraight.cs
+++ Assets/Script/Tile/RoadStraight.cs
@@ -9,16 +9,34 @@
         {
             List<Neighbor> neighbors = new List<Neighbor>();
 
+            Neighbor northStraightRoad = new Neighbor() { Offset = RotateOffset(1.0f, Neighbor.North), Tile = factory.RoadStraight, Rotation = CumulateRotation(0.0f) };
+
             Neighbor[] north = new Neighbor[] {
-                    new Neighbor() { Offset = RotateOffset(1.0f, Neighbor.North), Tile = factory.RoadStraight, Rotation = CumulateRotation(0.0f) },
+                    northStraightRoad,
+                    northStraightRoad,
+                    northStraightRoad,
+                    northStraightRoad,
+                    northStraightRoad,
+                    northStraightRoad,
+                    northStraightRoad,
+                    northStraightRoad,
                     new Neighbor() { Offset = RotateOffset(1.0f, Neighbor.North), Tile = factory.RoadCorner, Rotation = CumulateRotation(90.0f) },
                     new Neighbor() { Offset = RotateOffset(1.0f, Neighbor.North), Tile = factory.RoadCorner, Rotation = CumulateRotation(180.0f) }
                 };
 
             neighbors.Add(Pick(north));
 
+            Neighbor southStraightRoad = new Neighbor() { Offset = RotateOffset(1.0f, Neighbor.South), Tile = factory.RoadStraight, Rotation = CumulateRotation(0.0f) };
+
             Neighbor[] south = new Neighbor[] {
-                    new Neighbor() { Offset = RotateOffset(1.0f, Neighbor.South), Tile = factory.RoadStraight, Rotation = CumulateRotation(0.0f)},
+                    southStraightRoad,
+                    southStraightRoad,
+                    southStraightRoad,
+                    southStraightRoad,
+                    southStraightRoad,
+                    southStraightRoad,
+                    southStraightRoad,
+                    southStraightRoad,
                     new Neighbor() { Offset = RotateOffset(1.0f, Neighbor.South), Tile = factory.RoadCorner, Rotation = CumulateRotation(0.0f) },
                     new Neighbor() { Offset = RotateOffset(1.0f, Neighbor.South), Tile = factory.RoadCorner, Rotation = CumulateRotation(270.0f) }
                 };
